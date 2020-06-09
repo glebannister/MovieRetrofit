fe6a466e1f8f5b52a9b85f34645adc0f1c2589cb
Gleb <igi2000@inbox.ru>
2020-06-09 13:13:22 +0300

    init: create git rep

---
+++ app/src/main/java/android/glebannister/movieretrofit/main/MainActivity.java
@@ -0,0 +1,15 @@
+package android.glebannister.movieretrofit.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.glebannister.movieretrofit.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
